2424d445c1d93f51807a1089adb5a2ca40c49076
Lucas Pugnali <pugnali.lucas@gmail.com>
2019-09-15 21:44:19 -0300

    Creacion de app + mongo script

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.ML.Data;
+
+namespace BaBikes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    /// <summary>
+    /// The DemandObservation class holds one single bike demand observation record.
+    /// </summary>
+    public class DemandObservation
+    {
+        [LoadColumn(2)] public float Season { get; set; }
+        [LoadColumn(3)] public float Year { get; set; }
+        [LoadColumn(4)] public float Month { get; set; }
+        [LoadColumn(5)] public float Hour { get; set; }
+        [LoadColumn(6)] public float Holiday { get; set; }
+        [LoadColumn(7)] public float Weekday { get; set; }
+        [LoadColumn(8)] public float WorkingDay { get; set; }
+        [LoadColumn(9)] public float Weather { get; set; }
+        [LoadColumn(10)] public float Temperature { get; set; }
+        [LoadColumn(11)] public float NormalizedTemperature { get; set; }
+        [LoadColumn(12)] public float Humidity { get; set; }
+        [LoadColumn(13)] public float Windspeed { get; set; }
+        [LoadColumn(16)] [ColumnName("Label")] public float Count { get; set; }
+    }
+    
+    /// <summary>
+    /// The DemandPrediction class holds one single bike demand prediction.
+    /// </summary>
+    public class DemandPrediction
+    {
+        [ColumnName("Score")]
+        public float PredictedCount;
+    }
+}
---
+++ mongoQuery.js
@@ -0,0 +1,22 @@
+db.recorridos2019.aggregate( [ {
+    $project: {
+        bici_date:{
+        $let:{
+            vars:{
+                fecha: {$dateFromString: {
+                    dateString: '$bici_fecha_hora_retiro',
+                    timezone: 'UTC'
+                 }
+            }},
+            in:{
+              year: {$year:"$$fecha"},
+              month: {$month:"$$fecha"},
+              day: {$dayOfMonth: "$$fecha"},
+              hour: {$hour: "$$fecha"},
+              weekDay: {$dayOfWeek: "$$fecha"},
+              isWeekend: {$in:[{$dayOfWeek:"$$fecha"},[1,7]]}
+            }
+        }}
+    }},
+     {$match: { "bici_date.isWeekend": true }}
+    ] )
